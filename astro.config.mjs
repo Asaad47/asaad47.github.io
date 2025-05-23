527cff8b70963ddaa638004b9a2375bd14ee5a2d
Asaad47 <75583625+Asaad47@users.noreply.github.com>
2025-05-23 15:52:29 +0300

    Add files

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static',
+  site: 'https://asaad47.github.io/nikola-tesla',
+  base: '/nikola-tesla',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()],
+
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Geist",
+      cssVariable: "--font-geist",
+      fallbacks: ["Inter", "sans-serif"],
+    }]
+  }
+});
